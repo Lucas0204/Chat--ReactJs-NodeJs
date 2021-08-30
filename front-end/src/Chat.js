1b7106bdc23184b2eb88a72449997d255a06a255
Lucas Zordan <lucaszordan0204@gmail.com>
2021-08-30 14:23:32 -0300

    implementing

--- front-end/src/Chat.js
+++ front-end/src/Chat.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Messages from './components/Messages'
 import io from 'socket.io-client'
 
+export const socket = io('http://localhost:8080')
+
 export default function Chat() {
 
-    const socket = io('http://localhost:8080')
-
-    const [ message, setMessage ] = useState({})
+    const [ message, setMessage ] = useState('')
+    const [ messageToSend, setMessageToSend ] = useState({})
 
     function getMessage(event) {
         const message = event.target.value
-
-        
-        if (message) {
-            const messageTemplate = {
-                user: 'Lucas',
-                message
-            }
-
-            setMessage(messageTemplate)
-        }
+        setMessage(message)
     }
     
     function sendMessage() {
-    
-        if (message) {
-            socket.emit('sendMessage', message)
 
+        if (message.trim()) {
+            setMessageToSend({
+                user: 'Lucas',
+                message
+            })
+
+            setMessage('')
         }
     }
+
+    useEffect(() => {
+
+        if (messageToSend.message) {
+            socket.emit('sendMessage', messageToSend)
+        }
+
+    }, [ messageToSend ])
+
 
     return (
         <div className="chat-container">
 
-            <Messages></Messages>
+            <Messages message={messageToSend}></Messages>
 
             <div class="send-message">
-                <input onChange={getMessage} type="text" name="message" placeholder="Digite sua mensagem"/>
+                <input onChange={getMessage} type="text" value={message} name="message" placeholder="Digite sua mensagem"/>
                 <button onClick={sendMessage}>Enviar</button>
             </div>
 
---
+++ front-end/src/components/WelcomeMessage.js
@@ -0,0 +1,7 @@
+export default function WelcomeMessage() {
+    return (
+        <div id="welcome-message">
+            <p>Seja bem-vindo à sala de chat!</p>
+        </div>
+    )
+}
